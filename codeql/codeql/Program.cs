815c5ee358da8e5ecdf3f03edf30fb7ece54f9fe
sukrutgarita <sukrutgariya@gmail.com>
2026-02-28 00:30:28 +0800

    Add initial project setup with CI and .gitignore
    - Created a `.gitignore` file for Visual Studio project.
    - Added GitHub Actions workflow for build and SAST checks.
    - Created a new Visual Studio solution file `codeql.sln`.
    - Initialized `Program.cs` with a "Hello, World!" example.
    - Added project file `codeql.csproj` targeting .NET 8.0.

---
+++ codeql/codeql/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+const string s = "Hello, World! this is another comments";   
+Console.WriteLine(s);
+Console.ReadLine();
